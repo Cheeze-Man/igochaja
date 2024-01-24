cdc25c02a9cb51596e730c6d135503ea0da0930d
cheezeman <pksam5046@naver.com>
2024-01-24 14:37:11 +0900

    Add : 프로필 이미지 변경 기능 프로토타입

---
+++ src/api/EditUserInfo/UploadImageResponse/UploadImageResponse.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_BASE_URL;
+
+interface UploadImageResponse {
+  success: boolean;
+  message: string;
+}
+
+export const uploadProfileImage = async (
+  jwt: string,
+  imageFile: File,
+): Promise<UploadImageResponse> => {
+  try {
+    const formData = new FormData();
+    formData.append('image', imageFile);
+
+    const response = await axios.patch(
+      `${baseURL}/mypage/setting/image`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: jwt,
+        },
+      },
+    );
+
+    return response.data as UploadImageResponse;
+  } catch (error) {
+    alert(`프로필 이미지 변경 실패: ${error}`);
+    throw error;
+  }
+};
--- src/pages/MyPage/EditProfile/ProfileImage/ProfileImage.tsx
+++ src/pages/MyPage/EditProfile/ProfileImage/ProfileImage.tsx
@@ -1,11 +1,56 @@
+import { useState, ChangeEvent } from 'react';
+import { FaCamera } from 'react-icons/fa';
+import { uploadProfileImage } from 'api/EditUserInfo/UploadImageResponse/UploadImageResponse';
+import useUserStore from 'store/store';
 import './ProfileImage.scss';
 
 export default function ProfileImage() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const user = useUserStore((state) => state.user);
+  const accessToken = user?.token;
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file && file.type.startsWith('image/')) {
+      try {
+        const jwt = accessToken ? accessToken : 'TODO : 이 부분 지워야 함.';
+
+        const response = await uploadProfileImage(jwt, file);
+
+        if (response.success) {
+          window.location.reload();
+        } else {
+          alert(`프로필 이미지 업로드 실패:${response.message}`);
+        }
+      } catch (error) {
+        alert(`예상치 못한 오류:${error}`);
+      }
+    } else {
+      console.error('Please select a valid image file.');
+    }
+  };
+
   return (
-    <div>
+    <div className="profileImageDiv">
+      <label htmlFor="fileInput" className="cameraIconDiv shadow-xl">
+        <FaCamera className="cameraIcon" />
+        <input
+          type="file"
+          id="fileInput"
+          accept="image/*"
+          onChange={handleFileChange}
+          style={{ display: 'none' }}
+        />
+      </label>
       <img
         className="rounded-full size-96"
-        src="https://cvhrma.org/wp-content/uploads/2015/07/default-profile-photo.jpg"
+        src={
+          selectedFile
+            ? URL.createObjectURL(selectedFile)
+            : 'https://cvhrma.org/wp-content/uploads/2015/07/default-profile-photo.jpg'
+        }
         alt="profileImage"
       />
     </div>
